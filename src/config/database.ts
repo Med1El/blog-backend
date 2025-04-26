9f99f5690895ce786604fa7266fb32c33a7edaa0
Med1El <m3d13l@gmail.com>
2025-04-26 17:21:09 +0100

    getting rid off deprecated logged msgs in console

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 import process from 'process';
+import errorHandlerV2 from '../middlewares/errorHandlerV2';
+import logger from './logger';
 
 const connectDB = async () => {
     try {
+        mongoose.set('useNewUrlParser', true);
+        mongoose.set('useUnifiedTopology', true);
+        mongoose.set('useCreateIndex', true);
+        // mongoose.set('useFindAndModify', false);
         const conn = await mongoose.connect(process.env.MONGODB_URI as string);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error: any) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+        console.error(error);
     }
 };
 
